b6b03d84cda2afc1f9aabcca11c596a7579d46e8
aleksandrov <aaleksandrov@bears.software>
2020-07-30 13:01:46 +0300

    Updates

---
+++ precache-manifest.47b3d8dff4d064ee720416cf181584c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e5b2cde58cee1213936089a62ce377",
+    "url": "/usersm/index.html"
+  },
+  {
+    "revision": "bf183847612ccfe5a84d",
+    "url": "/usersm/static/css/main.d6b63799.chunk.css"
+  },
+  {
+    "revision": "4e3d0f122ee04935d603",
+    "url": "/usersm/static/js/2.42b9af3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/usersm/static/js/2.42b9af3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf183847612ccfe5a84d",
+    "url": "/usersm/static/js/main.78a08508.chunk.js"
+  },
+  {
+    "revision": "5895434ca97be4cca955",
+    "url": "/usersm/static/js/runtime-main.1ac7c600.js"
+  }
+]);
