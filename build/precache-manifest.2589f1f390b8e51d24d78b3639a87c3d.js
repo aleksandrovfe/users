44e35436cf2c471ec93a53898524e52d76a33c55
aleksandrov <aaleksandrov@bears.software>
2020-07-30 13:27:42 +0300

    fix image path

---
+++ build/precache-manifest.2589f1f390b8e51d24d78b3639a87c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b1cf099732446c60314f189c026a9a",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "9e2914ef212457b05ea1",
+    "url": "/users/static/css/main.34cff954.chunk.css"
+  },
+  {
+    "revision": "4e3d0f122ee04935d603",
+    "url": "/users/static/js/2.42b9af3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/users/static/js/2.42b9af3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2914ef212457b05ea1",
+    "url": "/users/static/js/main.72fab040.chunk.js"
+  },
+  {
+    "revision": "76492a2e6935e7af03e9",
+    "url": "/users/static/js/runtime-main.b4d70fad.js"
+  }
+]);
